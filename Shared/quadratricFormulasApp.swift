d3b1810cb8c85fd9544ff889b07e834328aa1361
Matt <madas@hawk.iit.edu>
2021-02-12 10:54:25 -0600

    Initial Commit

---
+++ Shared/quadratricFormulasApp.swift
@@ -0,0 +1,17 @@
+//
+//  quadratricFormulasApp.swift
+//  Shared
+//
+//  Created by Matthew Adas on 2/12/21.
+//
+
+import SwiftUI
+
+@main
+struct quadratricFormulasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
